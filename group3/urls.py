505dfc45e67d975f50b5260bfa7707bac5a5ceff
Peggy-Dong <p1233806272@gmail.com>
2021-06-01 00:19:06 +0800

    到admin完成

--- group3/urls.py
+++ group3/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mysite import views
+from mysite.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('date/', views.date),
-    path('', views.index),
+    path('',index),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.http import JsonResponse
 
 import datetime
 def index(request):
-    return HttpResponse("<h2>group3</h2>")
+	myname = "Recycle Home"
 
-def date(request):
-    return JsonResponse({'date':datetime.datetime.now()})
-# Create your views here.
+	return render(request, 'index.html', locals())
+ 
