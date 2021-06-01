00e7eb3fb2b11845a3a1b4b3ea7b28fefc150b25
Peggy-Dong <p1233806272@gmail.com>
2021-06-01 13:02:19 +0800

    不要動模板

--- group3/urls.py
+++ group3/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mysite.views import index
+from mysite.views import show
 
 urlpatterns = [
+	path('show<int:id>/', show),
     path('admin/', admin.site.urls),
     path('',index),
 ]
